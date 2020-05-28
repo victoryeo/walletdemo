0421fcfaf158f156016a43d771a01c5cf7c85cba
Victor Yeo <you@example.com>
2020-05-28 11:07:43 +0800

    add openwallet route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route, Switch } from "react-router-dom"
 import { AddWallet } from "./components/AddWallet"
+import { OpenWallet } from "./components/OpenWallet"
 import { NotFound } from "./components/NotFound"
 import { App } from "./App"
 
@@ -10,6 +11,7 @@ const Routes = () => {
       <Switch>
       <Route exact path="/" component={App} />
       <Route path="/addwallet" component={AddWallet} />
+      <Route path="/openwallet" component={OpenWallet} />
       <Route component={NotFound} />
       </Switch>
     </div>
