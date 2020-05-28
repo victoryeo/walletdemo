d328381a64438713c54044215784f56eab9e7cf5
Victor Yeo <you@example.com>
2020-05-28 10:58:41 +0800

    use constants

--- src/components/AddWallet.js
+++ src/components/AddWallet.js
@@ -4,11 +4,7 @@ import Button from 'react-bootstrap/Button'
 //import keyStore from  '../containers/keystore'
 import lightwallet from 'eth-lightwallet'
 import localStore from 'store/dist/store.modern'
-
-/* Default HD path string for key generation from seed */
-const hdPathString = `m/44'/60'/0'/0`
-/* keystore will be saved to local storage under this key */
-export const localStorageKey = 'ks';
+import { hdPathString, localStorageKey } from '../web3/constants'
 
 export class AddWallet extends Component {
   constructor(props) {
