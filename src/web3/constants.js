83b18c6de0968c7ff4f11fe249d52611ece91397
Victor Yeo <you@example.com>
2020-05-28 10:58:10 +0800

    add constants

---
+++ src/web3/constants.js
@@ -0,0 +1,4 @@
+/* Default HD path string for key generation from seed */
+export const hdPathString = `m/44'/60'/0'/0`
+/* keystore will be saved to local storage under this key */
+export const localStorageKey = 'ks';
