9c9d93fdf8c19552985db30ff9374d191bc974f6
Victor Yeo <you@example.com>
2020-05-28 14:27:40 +0800

    fix { App } import

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,16 @@ import { Route, Switch } from "react-router-dom"
 import { AddWallet } from "./components/AddWallet"
 import { OpenWallet } from "./components/OpenWallet"
 import { NotFound } from "./components/NotFound"
-import { App } from "./App"
+import App from "./App"
 
 const Routes = () => {
   return (
     <div>
       <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/addwallet" component={AddWallet} />
-      <Route path="/openwallet" component={OpenWallet} />
-      <Route component={NotFound} />
+        <Route exact path="/" component={App} />
+        <Route path="/addwallet" component={AddWallet} />
+        <Route path="/openwallet" component={OpenWallet} />
+        <Route component={NotFound} />
       </Switch>
     </div>
   )
