052adf445087ad0cbfb26005444de6eac872970c
Victor Yeo <you@example.com>
2020-05-30 13:33:26 +0800

    add ganache host

--- src/web3/constants.js
+++ src/web3/constants.js
@@ -1,4 +1,6 @@
 /* Default HD path string for key generation from seed */
 export const hdPathString = `m/44'/60'/0'/0`
 /* keystore will be saved to local storage under this key */
-export const localStorageKey = 'ks';
+export const localStorageKey = 'ks'
+
+export const ganachehost = 'http://localhost:7545'
